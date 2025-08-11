4524522e66b1f500371b68f13d594b6f9800819c
Rajan Jha <rajan.jha@iris-worldwide.com>
2025-08-11 19:07:06 +0530

    Remove db.php from tracking and add to .gitignore

--- includes/db.php
+++ includes/db.php
@@ -7,6 +7,7 @@ class dbconn {
     	$user = 'root';
     	$pass = '';
 
+        
         try {
             $db = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8", $user, $pass);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
