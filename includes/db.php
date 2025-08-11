c1cc4b7cd07070d09af0f382f42af639bfb6e6f9
Rajan Jha <rajan.jha@iris-worldwide.com>
2025-08-11 19:03:32 +0530

    Initial commit

---
+++ includes/db.php
@@ -0,0 +1,22 @@
+<?php
+class dbconn {
+    public function connect() {
+        // localhost connection
+    	$host = 'localhost';
+    	$dbName = 'immersive-flipbook_db';
+    	$user = 'root';
+    	$pass = '';
+
+        try {
+            $db = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8", $user, $pass);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $db;
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+}
+
+
+?>
+
