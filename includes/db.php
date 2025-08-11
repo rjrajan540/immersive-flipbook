ad94f858882adffc05fa8cbf1b1ead804019a2f8
Rajan Jha <111979059+rjrajan540@users.noreply.github.com>
2025-08-11 17:57:03 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,17 @@
 <?php
 class dbconn {
     public function connect() {
+        // localhost connection
+    	// $host = 'localhost';
+    	// $dbName = 'immersive-flipbook_db';
+    	// $user = 'root';
+    	// $pass = '';
 
-    	$host = 'localhost';
-    	$dbName = 'immersive-flipbook_db';
-    	$user = 'root';
-    	$pass = '';
+        // server connection
+        $host = 'sql110.infinityfree.com';
+    	$dbName = 'if0_39680559_immersiveFlipbookDB';
+    	$user = 'if0_39680559';
+    	$pass = 'Dwdsl1105';
 
         try {
             $db = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8", $user, $pass);
